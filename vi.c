d364097f4346ab6b74a18896fa4ebc2e12e9cd4b
Kishore02721 <kishore.k@yantravision.com>
2024-12-16 16:48:20 +0530

    just uploading this as i am bored

---
+++ vi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{char option;
+printf("Writing this code as i am just bored to do anything.Are you also bored. If yes press y else n :");
+scanf("%c",&option);
+if(option == 'y')
+printf("Super!Another fool bored like me. Do something useful, go...");
+if(option == 'n')
+printf("If you are really not bored then why reading this idiot. Go do something better");
+if(option!='y'&&option!='n')
+printf("Hey fool, cant you press even character correctly. Just go and do something meaningful");
+return 0;}
